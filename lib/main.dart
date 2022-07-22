ea344456beeb769af16e7a2cf0a6fe420111ab86
lovefreeUnity <jo-ro3430@naver.com>
2022-07-22 18:57:26 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('다각형 그리기'),
+            bottom: const TabBar(tabs: [
+              Tab(text: '앱 개발환경 세팅'),
+              Tab(text: '입력값'),
+              Tab(text: 'N각형')
+            ],
+            ),
+          ),
+          body: TabBarView(
+              children: <Widget>[
+                const Center(
+                    child: Text(
+                      'Hello Everex',
+                      style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.black
+                      ),
+                    )
+                ),
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text('N을 입력해주세요.'),
+                      TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
+                      //패치로 인해 2년전과 버튼 스타일 선언이 바뀜
+                      ElevatedButton(
+                        onPressed:null,
+                        child: Text(
+                          '확인',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white
+                          ),
+                        ),
+                      )
+                    ]
+                ),
+                const Text('3'),
+              ]
+          ),
+        ),
+      ),
+    );
+  }
+}
